fc1629e858eb307f8ccb588099b715f67cdaa326
Ricardo Ivan Lozano Hipolito <Ricardo-Ivan.Lozano@endava.com>
2025-08-19 13:09:02 -0600

    test: update greet test to match new greeting format

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,8 @@ from src.app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World!")
+            self.assertEqual(greet("World"), "Hello, World! Welcome to DevLake Demo Project for Endava")
+
 
 if __name__ == "__main__":
     unittest.main()
